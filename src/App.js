156828b468bda3ba94146a6f02d28ee45d3f93e5
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-12-02 14:24:15 -0800

    invoking callback from child component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import SearchBar from './components/SearchBar'
 
 class App extends Component {
+
+  onSearchSubmit(term) {
+    console.log(term);
+  }
   render() {
     return (
       <div className="App ui container" style={{ marginTop: '20px' }}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,7 +14,7 @@ class SearchBar extends Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state.term)
+    this.props.onSubmit(this.state.term);
   }
 
   render() {
