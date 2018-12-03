a0801091751b3bc6a6049b42f3eec5268feaa6a8
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-12-02 20:29:55 -0800

    create ref, get acces to each image height

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,30 @@
+//ref
+
 import React, { Component } from 'react'
 
 class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    // this is how we create ref
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  }
+
   render() {
 
     const { description, urls } = this.props.image
     return (
       <div>
         <img
+          ref={this.imageRef}
           alt={description}
           src={urls.regular}
         />
